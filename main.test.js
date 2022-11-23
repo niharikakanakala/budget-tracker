7612ca57b302f5bdc9e38b1f4e93d0c195b4a066
Niharika Kanakala <niharika.k@aithinkers.com>
2022-11-23 14:20:32 +0530

    sample tests

--- main.test.js
+++ main.test.js
@@ -3,32 +3,42 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { fireEvent } from '@testing-library/dom';
 import '@testing-library/jest-dom';
-
-
+import { sum } from './src/utils';
+import { TransactionItem } from './src/components/transaction-list/transaction-item/transaction-item';
 import { TransactionList } from './src/components/transaction-list/transaction-list';
 import { Form } from './src/components/form/form';
 
 
 
-// describe('<TransactionItem>', () => {
 
-//   test('calls onDeleteClick with transaction id', () => {
-//     const onDeleteClick = jest.fn();
-//     render(
-//       <TransactionItem
-//         id="test-id"
-//         description="Apple"
-//         value={10}
-//         type="expense"
-//         onDeleteClick={onDeleteClick}
-//       />
-//     );
+describe('sum', () => {
+  it('correctly sums elements of array', () => {
+    expect(sum([1, 2, 3])).toBe(6);
+    expect(sum([])).toBe(0);
+    expect(sum([-2, 3])).toBe(1);
+  });
+});
 
-//     userEvent.click(screen.getByRole('button'));
 
-//     expect(onDeleteClick).toBeCalledWith('test-id');
-//   });
-// });
+describe('<TransactionItem>', () => {
+
+  test('calls onDeleteClick with transaction id', () => {
+    const onDeleteClick = jest.fn();
+    render(
+      <TransactionItem
+        id="test-id"
+        description="Apple"
+        value={10}
+        type="expense"
+        onDeleteClick={onDeleteClick}
+      />
+    );
+
+    userEvent.click(screen.getByRole('button'));
+
+    expect(onDeleteClick).toBeCalledWith('test-id');
+  });
+});
 
 const incomeList = [
   { id: 'a', type: 'income', description: 'Salary', value: 999 },
