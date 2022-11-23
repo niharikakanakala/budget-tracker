1a1567f56798ba3d407b72b2ab654213a0c887f7
Niharika Kanakala <niharika.k@aithinkers.com>
2022-11-23 14:01:06 +0530

    test cases

--- sample.test.js
+++ sample.test.js
@@ -1,10 +1,35 @@
-describe('frontend sample tests', () => {
-    it('should always be used run some simple test or the candidate should be allowed to modify sample.test.js file', (done) => {
-      try {
-        expect(true).toBe(true);
-        done();
-      } catch (error) {
-        done(error);
-      }
-    });
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { fireEvent } from '@testing-library/dom';
+import '@testing-library/jest-dom';
+import { sum } from './src/utils';
+import { TransactionItem } from './src/components/transaction-list/transaction-item/transaction-item';
+
+describe('sum', () => {
+  it('correctly sums elements of array', () => {
+    expect(sum([1, 2, 3])).toBe(6);
+    expect(sum([])).toBe(0);
+    expect(sum([-2, 3])).toBe(1);
   });
+});
+
+describe('<TransactionItem>', () => {
+
+  test('calls onDeleteClick with transaction id', () => {
+    const onDeleteClick = jest.fn();
+    render(
+      <TransactionItem
+        id="test-id"
+        description="Apple"
+        value={10}
+        type="expense"
+        onDeleteClick={onDeleteClick}
+      />
+    );
+
+    userEvent.click(screen.getByRole('button'));
+
+    expect(onDeleteClick).toBeCalledWith('test-id');
+  });
+});
